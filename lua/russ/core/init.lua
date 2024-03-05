53244c8a4879b586cbccedbbe1a752e268af3ff5
Russ Goetz <russgoetz@gmail.com>
2024-03-04 19:38:41 -0500

    Markdown files now wrap, added markdown preview plugin

--- lua/russ/core/init.lua
+++ lua/russ/core/init.lua
@@ -1,2 +1,9 @@
 require("russ.core.options")
 require("russ.core.keymaps")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*.md",
+    callback = function(ev)
+        vim.opt_local.wrap = true
+    end
+})
--- lua/russ/plugins/telescope.lua
+++ lua/russ/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = {
-        "nvim-lua/plenary.nvim",
+        { "nvim-lua/plenary.nvim" },
         {
             "nvim-telescope/telescope-fzf-native.nvim",
             build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
